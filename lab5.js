16adcaef0cd86da2896e84096aa1282e12fe8d71
CentennialR <zimnnadozie@gmail.com>
2023-11-10 11:21:20 -0500

    add

--- lab5.js
+++ lab5.js
@@ -1,7 +1,15 @@
 class CenterHtml extends HTMLElement{
     connectCallback(){
-        this.innerHTML = `<div style=text-align:center">${this.innerHTML}</div>`;
+        this.innerHTML = `<div style="text-align:center">${this.innerHTML}</div>`;
     }
 }
 
 customElements.define("x-center", CenterHtml);
+
+class BlueHtml extends HTMLElement{
+    connectCallback(){
+        this.innerHTML = `<div style="color:blue">${this.innerHTML}</div>`;
+    }
+}
+
+customElements.define("x-blue", Blue.html);
